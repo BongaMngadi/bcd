202100aac267f046db1894ed9e27a95f21ce58fd
BongaConnection <dlokovu100@gmail.com>
2023-03-28 03:29:44 +0200

    last changes

--- packages/frontend/utils/config.ts
+++ packages/frontend/utils/config.ts
@@ -1,5 +1,5 @@
-const API_PRODUCTION_URL = 'https://api.getorca.org';
-const API_DEV_URL = 'http://localhost:4000';
+const API_PRODUCTION_URL = 'https://bcd.onrender.com';
+const API_DEV_URL = 'https://bcd.onrender.com';
 
 export const Config = {
   API_URL: process.env.NODE_ENV === 'production' ? API_PRODUCTION_URL : API_DEV_URL,
